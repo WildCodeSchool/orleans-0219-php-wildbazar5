4c1622b44c893b69a6813b92c088e9aaba31dd61
Vang Jeannette <vangjeannette@gmail.com>
2019-03-13 17:59:29 +0100

    ajout des fichiers kitchen.php avec le tableau et la boucle modal et le fichier data_kitchen.php

---
+++ data_kitchen.php
@@ -0,0 +1,103 @@
+<?php
+$tableProducts = [
+    'prod0001' => ['Mug Wild Code School Logo', '19.90', 'White ceramic mug with the red Wild Code School\'s logo. Delivred in a protective packaging. Compatible with dishwasher and microwaves.', 'assets/img/kitchen/mug1.jpg','1,1 lbs', 'H 12,5 x W 8', 'Ceramic', 'K64989' ] ,
+    'prod0002' => ['Mug Wild Code School Logo Deer', '19.90', 'White ceramic mug with the Deer Wild Code School\'s logo. Delivred in a protective packaging. Compatible with dishwasher and microwaves.', 'assets/img/kitchen/mug2.png', '1,1 lbs', 'H 12,5 x W 8', 'Ceramic', 'K64991'],
+    'prod0003' => ['Mug Dinosaur Google', '19.90', 'White ceramic mug with the famous Google Dinosaur. Delivred in a protective packaging. Compatible with dishwasher and microwaves.', 'assets/img/kitchen/front.png', '1,1 lbs', 'H 12,5 x W 8', 'Ceramic', 'K66321'] ,
+    'prod0004' => ['Mug Geek Joke', '19.90', 'White ceramic mug designed with joke about geek. Delivred in a protective packaging. Compatible with dishwasher and microwaves.', 'assets/img/kitchen/muggeek.jpeg', '1,1 lbs', 'H 12,5 x W 8', 'Ceramic', 'K66334'] ,
+    'prod0005' => ['Mug Star Wars R2D2', '19.90', 'White ceramic mug with black handle, designed with the famous character from Star Wars : R2D2. Delivred in a protective packaging. Compatible with dishwasher and microwaves.', 'assets/img/kitchen/mug-geek-parodie-star-wars-r2-je-suis-to.jpg', '1,1 lbs', 'H 12,5 x W 8', 'Ceramic', 'K66347'] ,
+    'prod0006' => ['Mug Design won\'t save the world', '19.90', 'Black ceramic mug about design. Delivred in a protective packaging. Compatible with dishwasher and microwaves.', 'assets/img/kitchen/mugdesign01.png', '1,1 lbs', 'H 12,5 x W 8', 'Ceramic', 'K66354'] ,
+    'prod0007' => ['Mug Aestetic Silver', '19.90', 'White ceramic mug with silver pattern. Delivred in a protective packaging. Compatible with dishwasher and microwaves.', 'assets/img/kitchen/mugdesign02.jpg', '1,1 lbs', 'H 9,5 x W 8', 'Ceramic', 'K66366'] ,
+    'prod0008' => ['Mug Aestetic Gold', '19.90', 'White ceramic mug with gold pattern. Delivred in a protective packaging. Compatible with dishwasher and microwaves.', 'assets/img/kitchen/mugdesign03.jpg', '1,1 lbs', 'H 9,5 x W 8', 'Ceramic', 'K66373'] ,
+    'prod0009' => ['Mug Pacman Black', '19.90', 'Black ceramic mug with a printed design from the famous game Pacman. Delivred in a protective packaging. Compatible with dishwasher and microwaves.', 'assets/img/kitchen/mugpacman.jpg', '1,1 lbs', 'H 12,5 x W 8', 'Ceramic', 'K66380'] ,
+    'prod0010' => ['Mug Algorithm Of Success Black', '19.90', 'White ceramic mug with a coding printed design. Delivred in a protective packaging. Compatible with dishwasher and microwaves.', 'assets/img/kitchen/mugprog.jpg', '1,1 lbs', 'H 12,5 x W 8', 'Ceramic', 'K66399' ] ,
+    'prod0011' => ['Mug Pusheen', '19.90', 'White ceramic mug with a printed design from the famous cat Pusheen. Delivred in a protective packaging. Compatible with dishwasher and microwaves.', 'assets/img/kitchen/mugpusheen.jpg', '1,1 lbs', 'H 12,5 x W 8', 'Ceramic', 'K6640'] ,
+    'prod0012' => ['Mug Retro Gaming Gameboy', '19.90', 'White ceramic mug with a printed design from the famous retro gaming Nintendo. Delivred in a protective packaging. Compatible with dishwasher and microwaves.', 'assets/img/kitchen/mugretrogeek.jpg', '1,1 lbs', 'H 12,5 x W 8', 'Ceramic', 'K66415'] ,
+    'prod0013' => ['Ecocup Logo Deer Wild Code School', '5.90', 'White ecocup made with recycled plastic and printed with the Wild Code School\'s deer logo. Compatible with dishwasher and microwaves.', 'assets/img/kitchen/gob1.png', '0,350 lbs', 'H 33,3 x W 7,5', 'Recycled plastic', 'K75328'] ,
+    'prod0014' => ['Ecocup Logo Wild Code School', '5.90', 'White ecocup made with recycled plastic and printed with the Wild Code School\'s red logo. Compatible with dishwasher and microwaves.', 'assets/img/kitchen/gob2.png', '0,350 lbs', 'H 33,3 x W 7,5', 'Recycled plastic', 'K75331'] ,
+    'prod0015' => ['Ecocup Assassin\'s Creed White', '5.90', 'White ecocup made with recycled plastic and printed with the Assassin\'s Creed logo. Compatible with dishwasher and microwaves.', 'assets/img/kitchen/gobas.jpg', '0,350 lbs', 'H 33,3 x W 7,5', 'Recycled plastic', 'K75341' ] ,
+    'prod0016' => ['Ecocup Bethesda Vault Boy','5.90', 'Transparent ecocup made with recycled plastic and printed with Vault Boy from Bethesda. Compatible with dishwasher and microwaves.', 'assets/img/kitchen/gobfallout.jpg', '0,350 lbs', 'H 33,3 x W 7,5', 'Recycled plastic', 'K75357'] ,
+    'prod0017' => ['Ecocup Gaming Link', '5.90','Blue ecocup made with recycled plastic and printed with Link from the famous game Zelda. Compatible with dishwasher and microwaves.', 'assets/img/kitchen/goblink.jpg', '0,350 lbs', 'H 33,3 x W 7,5', 'Recycled plastic', 'K75369'] ,
+    'prod0018' => ['Ecocup Daryl The Walking Dead', '5.90', 'Black and red ecocup made with recycled plastic and printed with Daryl from The Walking Dead. Compatible with dishwasher and microwaves.', 'assets/img/kitchen/gobwd.jpg', '0,350 lbs', 'H 33,3 x W 7,5', 'Recycled plastic', 'K75375'] ,
+    'prod0019' => ['Bowl Logo Wild Code School Deer', '10.90', 'White ceramic bowl with the Deer Wild Code School\'s logo. Delivred in a protective packaging. Compatible with dishwasher and microwaves.', 'assets/img/kitchen/bol1.png', '1,3 lbs', 'H 13 x W 13', 'Ceramic', 'K86232'] ,
+    'prod0020' => ['Bowl Logo Wild Code School', '10.90', 'White ceramic bowl with the red Wild Code School\'s logo. Delivred in a protective packaging. Compatible with dishwasher and microwaves.', 'assets/img/kitchen/bol2.png', '1,3 lbs', 'H 13 x W 13', 'Ceramic', 'K86243']
+];
+
+?>
+           <!-- PRODUIT -->
+    <?php foreach($tableProducts as $nameProduct=>$valueProduct) { ?>
+            <div class="col-md-6 col-lg-4 col-xl-3">
+                <article >
+                    <div class="card border-dark my-4 dr_card" data-toggle="modal" data-target= #<?php  echo $nameProduct ; ?>>
+                        <div class="dr_wrapper text-center ">
+                            <img src= <?php  echo $valueProduct[3] ; ?> class="card-img-top img-fluid art-image"
+                                alt="mug wild code school1">
+                        </div>
+                        <div class="card-img-overlay">
+                            <div class="card-body text-right p-1">
+                                <h3><span class="card-text badge badge-info dr_badge"><?php  echo $valueProduct[1]; ?> $</span></h3>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <h4 class="card-title text-center"><?php  echo $valueProduct[0] ; ?> </h4>
+                        </div>
+                    </div>
+                </article>
+
+            </div>
+
+        <div class="modal fade" id= <?php  echo $nameProduct ; ?> tabindex="-1" role="dialog" aria-labelledby="modalDescrProduct"
+             aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title" id="exampleModalLabel"><?php  echo $valueProduct[0] ; ?></h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row no-gutters">
+                            <div class="col-xs-6 col-md-4 art-img-content">
+                                <img class="card-img art-img" src= <?php  echo $valueProduct[3] ; ?> alt="Card image" />
+                            </div>
+                            <div class=" col-xs-6 col-md-8">
+                                <div class="card-body">
+                                    <h4><span class="art-price"></span></h4>
+                                    <p class="card-text art-descr"></p>
+                                    <table class="table table-striped table-sm">
+                                        <tbody>
+                                        <tr>
+                                            <th scope="row">Weight</th>
+                                            <td><?php  echo $valueProduct[4] ; ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">Size (inches) :</th>
+                                            <td><?php  echo $valueProduct[5] ; ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">Main material : </th>
+                                            <td><?php  echo $valueProduct[6] ; ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">Reference</th>
+                                            <td><?php  echo $valueProduct[7] ; ?></td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer row  justify-content-end p-2 m-1 text-center">
+                        <div class="col-sm-6 col-md-4 col-lg-3 p-2">
+                            <button type="button" class="btn btn-danger  w-100" data-dismiss="modal">Close</button>
+                        </div>
+                        <div class="col-sm-6  col-md-4 col-lg-3 p-2">
+                            <button type="button" class="btn btn-info w-100">+ Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+<?php } ; ?>
