d449b6d90a27c666953e676777db4a2b9b9b3969
Damien Rambourg <damien.rambourg@gmail.com>
2019-03-13 09:47:32 +0100

    Ajout du header en Php dans l'index et création du header.php

---
+++ header.php
@@ -0,0 +1,45 @@
+
+<header id="nawalNavbar">
+            <nav class="navbar navbar-expand-lg">
+                <a class="navbar-brand ml-5" href="index.html">
+                    <img src="assets/img/logo/logo-rouge-fond-transparent.png" class="d-inline-block align-top" alt="logo de la wild code school" /></a>
+
+                <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item">
+                            <a class="link-1" href="clothes.html">Clothes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="link-1" href="goodies.html">Goodies</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="link-1" href="kitchen.html">Kitchen</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="link-1" href="slippers.html">Slippers</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="link-1" href="figurines.html">Figurines</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="link-1" href="#aboutus">About Us</a>
+                        </li>
+                    </ul>
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+            </nav>
+
+            <div class="jumbotron paral paralsec m-0">
+                <div class="container text-center">
+                    <h1 class="display-4">WILD BAZAR</h1>
+                    <p class="lead">Find everything and espescially anything !</p>
+                </div>
+            </div>
+        </header>
