5b322a997848af4ffd585698cb0c84fc2c0851df
Damien Rambourg <damien.rambourg@gmail.com>
2019-03-13 16:21:40 +0100

    Suppression du module JS pour les modals

--- goodies.php
+++ goodies.php
@@ -117,7 +117,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
         crossorigin="anonymous"></script>
-<!--    <script src="assets/js/modal.js"></script>-->
+
     <!-- Script d'effet de texte de titre majeur -->
     <script src="assets/js/scroll.js"></script>
 
