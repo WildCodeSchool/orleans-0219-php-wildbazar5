061f56d6e0c69250a59c2523f4ae8ad205157f01
Vang Jeannette <vangjeannette@gmail.com>
2019-03-15 12:23:25 +0100

    modification final du tableau data_kitchen.php et include card.php sur fichier kitchen.php

--- kitchen.php
+++ kitchen.php
@@ -29,88 +29,12 @@ php<!DOCTYPE html>
 
         <div class="row p-2">
             <?php
-            include "data_kitchen.php" ;
+            include "assets/data_kitchen.php";
+            include "assets/cards.php" ;
             ?>
             <!-- PRODUIT -->
 
-            <!-- PRODUIT -->
-            <?php foreach($tableProducts as $nameProduct=>$valueProduct) { ?>
-                <div class="col-md-6 col-lg-4 col-xl-3">
-                    <article >
-                        <div class="card border-dark my-4 dr_card" data-toggle="modal" data-target= #<?php  echo $nameProduct ; ?>>
-                            <div class="dr_wrapper text-center ">
-                                <img src= <?php  echo $valueProduct[3] ; ?> class="card-img-top img-fluid art-image"
-                                alt="mug wild code school1">
-                            </div>
-                            <div class="card-img-overlay">
-                                <div class="card-body text-right p-1">
-                                    <h3><span class="card-text badge badge-info dr_badge"><?php  echo $valueProduct[1]; ?> $</span></h3>
-                                </div>
-                            </div>
-                            <div class="card-body">
-                                <h4 class="card-title text-center"><?php  echo $valueProduct[0] ; ?> </h4>
-                            </div>
-                        </div>
-                    </article>
 
-                </div>
-
-                <div class="modal fade" id= <?php  echo $nameProduct ; ?> tabindex="-1" role="dialog" aria-labelledby="modalDescrProduct"
-                     aria-hidden="true">
-                    <div class="modal-dialog modal-lg" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h4 class="modal-title" id="exampleModalLabel"><?php  echo $valueProduct[0] ; ?></h4>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="row no-gutters">
-                                    <div class="col-xs-6 col-md-4 art-img-content">
-                                        <img class="card-img art-img" src= <?php  echo $valueProduct[3] ; ?> alt="Card image" />
-                                    </div>
-                                    <div class=" col-xs-6 col-md-8">
-                                        <div class="card-body">
-                                            <h4><span class="art-price"></span></h4>
-                                            <p class="card-text art-descr"></p>
-                                            <table class="table table-striped table-sm">
-                                                <tbody>
-                                                <tr>
-                                                    <th scope="row">Weight</th>
-                                                    <td><?php  echo $valueProduct[4] ; ?></td>
-                                                </tr>
-                                                <tr>
-                                                    <th scope="row">Size (inches) :</th>
-                                                    <td><?php  echo $valueProduct[5] ; ?></td>
-                                                </tr>
-                                                <tr>
-                                                    <th scope="row">Main material : </th>
-                                                    <td><?php  echo $valueProduct[6] ; ?></td>
-                                                </tr>
-                                                <tr>
-                                                    <th scope="row">Reference</th>
-                                                    <td><?php  echo $valueProduct[7] ; ?></td>
-                                                </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="modal-footer row  justify-content-end p-2 m-1 text-center">
-                                <div class="col-sm-6 col-md-4 col-lg-3 p-2">
-                                    <button type="button" class="btn btn-danger  w-100" data-dismiss="modal">Close</button>
-                                </div>
-                                <div class="col-sm-6  col-md-4 col-lg-3 p-2">
-                                    <button type="button" class="btn btn-info w-100">+ Add to cart</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            <?php } ; ?>
 
         </div>
 
@@ -159,60 +83,6 @@ php<!DOCTYPE html>
         </div>
     </div>
 
-    <div class="modal fade" id="modalDescrProduct" tabindex="-1" role="dialog" aria-labelledby="modalDescrProduct"
-        aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title" id="exampleModalLabel"></h4>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="row no-gutters">
-                        <div class="col-xs-6 col-md-4 art-img-content">
-                            <img class="card-img art-img" src="assets/img/slippers/no-image-icon-15.png" alt="Card image" />
-                        </div>
-                        <div class=" col-xs-6 col-md-8">
-                            <div class="card-body">
-                                <h4><span class="art-price"></span></h4>
-                                <p class="card-text art-descr"></p>
-                                <table class="table table-striped table-sm">
-                                    <tbody>
-                                        <tr>
-                                            <th scope="row">Weight</th>
-                                            <td><span class="art-carac1"></span></td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">Size (inches) :</th>
-                                            <td><span class="art-carac2"></span></td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">Main material : </th>
-                                            <td><span class="art-carac3"></span></td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">Reference</th>
-                                            <td><span class="art-carac6"></span></td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer row  justify-content-end p-2 m-1 text-center">
-                    <div class="col-sm-6 col-md-4 col-lg-3 p-2">
-                        <button type="button" class="btn btn-danger  w-100" data-dismiss="modal">Close</button>
-                    </div>
-                    <div class="col-sm-6  col-md-4 col-lg-3 p-2">
-                        <button type="button" class="btn btn-info w-100">+ Add to cart</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
     <!-- Footer -->
     <?php
     include "footer.php" ;
