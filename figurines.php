85f9217004b1ca7873810d6c5cbed2c6d6daaa0d
Shirley Bismuth <shirley.bsth@gmail.com>
2019-03-15 10:08:15 +0100

    partie complétée

---
+++ figurines.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300i,700" rel="stylesheet" />
+    <link rel="stylesheet" href="assets/stylefonts.css" />
+    <link rel="stylesheet" href="assets/style.css" />
+    <title>Wild Bazar</title>
+</head>
+
+<body>
+
+    <div class="container-fluid p-0">
+        <?php
+        include 'header.php'
+        ?>
+    </div>
+
+    <div class="container p-0">
+        <!-- Section Top Selling -->
+        <section class="dr_section_color">
+
+            <div class="row p-2">
+                    <!-- PRODUIT -->
+                    
+                    <?php include 'tableaufig.php';
+              foreach ($typeAndAnswer as $key => $value) {?>
+
+                    <div class="col-md-6 col-lg-4 col-xl-3">
+                            <article>
+                                <div class="card border-dark my-4 dr_card" data-toggle="modal" data-target="<?= '#' . $key ?>">
+                                <div class="dr_wrapper text-center ">
+                                        <img src="<?= $value['URLimg']?>"
+                                            class="card-img-top img-fluid art-image" alt="Produit 4">
+                                </div>
+                                <div class="card-img-overlay">
+                                    <div class="card-body text-right p-1">
+                                            <h3><span class="card-text badge badge-info dr_badge"><?= $value['Value'] ?> $</span></h3>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                        <h4 class="card-title text-center"><?= $value['Title']; ?></h4>
+                                </div>
+                                </div>
+                            </article>
+                    </div>
+
+
+
+    
+
+    <!-- END ROW -->
+
+
+
+    <div class="modal fade" id=<?= $key ?> tabindex="-1" role="dialog" aria-labelledby="modalDescrProduct"
+        aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="exampleModalLabel"></h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <div class="modal-body">
+                    <div class="row no-gutters">
+                        <div class="col-xs-6 col-md-4 art-img-content">
+                            <img class="card-img art-img" src="<?= $value['URLimg']?>" alt="Card image" />
+                        </div>
+                        <div class=" col-xs-6 col-md-8">
+                            <div class="card-body">
+                                <h4><span class="art-price"><?= $value['Value'] ?> $</span></h4>
+                                <p class="card-text art-descr"><?= $value['Description'] ?></p>
+                                <table class="table table-striped table-sm">
+                                    <tbody>
+                                        <tr>
+                                          <th scope="row">Height</th>
+                                          <td><span><?= $value['Height'] ?> Inch</span></td>
+                                        </tr>
+                                        <tr>
+                                          <th scope="row">Weight</th>
+                                          <td><span><?= $value['Weight'] ?> Lbs</span></td>
+                                        </tr>
+                                        <tr>
+                                          <th scope="row">Color</th>
+                                          <td><span><?= $value['Color'] ?></span></td>
+                                        </tr>
+                                        <tr>
+                                          <th scope="row">Reference</th>
+                                          <td><span><?= $value['Ref'] ?></span></td>
+                                        </tr>
+                                  </tbody>
+                                </table>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="modal-footer row  justify-content-end p-2 m-1 text-center">
+                    <div class="col-sm-6 col-md-4 col-lg-3 p-2">
+                        <button type="button" class="btn btn-danger  w-100" data-dismiss="modal">Close</button>
+                    </div>
+                    <div class="col-sm-6  col-md-4 col-lg-3 p-2">
+                        <button type="button" class="btn btn-info w-100">+ Add to cart</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+
+        <?php } ?>
+            </div>
+        </section>
+    </div>
+
+        <!-- DEFINITION DE FENETRE MODALE LEGAL MENTIONS-->
+        <div class="modal fade" id="modalLegalMention" tabindex="-1" role="dialog"
+             aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title" >Legal Mentions</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row no-gutters">
+                            <div class="col-xs-6 col-md-4 art-img-content">
+                                <img class="card-img" src="assets/img/logo/yoshi.png" alt="Card image" />
+                            </div>
+                            <div class=" col-xs-6 col-md-8">
+                                <div class="card-body wildbazar-titlemodal">
+                                    <h2>WILD BAZAR</h2>
+                                    <p class="card-text art-descr">Images and data on this site are subject to copyright.</p>
+                                    <p class="card-text art-descr">Some images used come from proprietary website and must be used after
+                                        acceptance of the owner.</p>
+                                    <h3>© Yoshi Corporate - Wild Code Scool 2019</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer row  justify-content-end p-2 m-1 text-center">
+                        <div class="col-sm-6 col-md-4 col-lg-3 p-2">
+                            <button type="button" class="btn btn-danger  w-100" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+  <?php
+  include 'footer.php' ?>
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+    <script src="assets/js/modal.js"></script>
+    <!-- Script d'effet de texte de titre majeur -->
+    <script src="assets/js/scroll.js"></script>
+
+</body>
+
+</html>
---
+++ tableaufig.php
@@ -0,0 +1,20 @@
+<?php
+$typeAndAnswer = [
+     'Produit1'=> ['Title'=>'Pink deer','Value' => 39.99, 'Description' =>'Pink deer official plushtoy of the WCS','URLimg' => 'https://i.imgur.com/1g4FSXj.jpg', 'Height' => 40, 'Weight' => 1.5, 'Color'=>'Pink','Ref' =>'S74589' ],
+    'Produit2'=>['Title'=>'Pink deer','Value' => 29.99, 'Description' =>'Pink deer official plushtoy of the WCS','URLimg' => 'https://i.imgur.com/NUkUBrz.jpg', 'Height' => 35, 'Weight' => 0.5, 'Color'=>'Pink','Ref' =>'M78445' ],
+    'Produit3'=>['Title'=>'Red deer','Value' => 32.00, 'Description' =>'Red deer official plushtoy of the WCS','URLimg' => 'https://i.imgur.com/kWIbRsq.jpg', 'Height' => 30, 'Weight' => 1.0, 'Color'=>'Red','Ref' =>'K7763' ],
+    'Produit4'=>['Title'=>'White Deer','Value' => 29.99, 'Description' =>'Little white deer 100% cotton and soft','URLimg' => 'https://i.imgur.com/yymGkE5.jpg', 'Height' => 3, 'Weight' => 1.9, 'Color'=>'White','Ref' =>'N53984' ],
+    'Produit5'=>['Title'=>'Rainbow lama bag','Value' => 49.99, 'Description' =>'Wonderful rainbow lama bag for school or work !','URLimg' => 'http://www.eispristic.fr/images/cate_3/640/Remise-Sur-Vente-Candice-guo-Super-mignon-en-peluche-jouet-beau-dessin-anim%C3%A9-arcenArpakasso-alpaga-en-peluche-poup%C3%A9e-souple-sac-%C3%A0-dos-sac-danniversaire-cadeau-De-No%C3%ABl-WJH3899-god0.jpg', 'Height' => 20, 'Weight' => 3.0, 'Color'=>'Rainbow','Ref' =>'G51223' ],
+    'Produit6'=>['Title'=>'PlushToy Yoda','Value' => 59.99, 'Description' =>'PlushToy Yoda','URLimg' => 'https://i.imgur.com/2cQHgR6.jpg', 'Height' => 40, 'Weight' => 1.5, 'Color'=>'Yoda Colors','Ref' =>'K87745' ],
+    'Produit7'=>['Title'=>'SpongBob','Value' => 19.99, 'Description' =>'Who lives in a pineapple under the sea : SPONGBOB SQUARE PANTS','URLimg' => 'https://boutique.plushtoy.fr/WebRoot/ce_fr/Shops/186372/4CE0/3BAC/86B8/EC16/296E/C0A8/8007/F5B4/peluche-bob-leponge-lunett_ml.jpg', 'Height' => 15, 'Weight' => 1.5, 'Color'=>'Yellow','Ref' =>'D92256' ],
+    'Produit8'=>['Title'=>'Aviator Pepe','Value' => 39.99, 'Description' =>'Find me on Dalaran and i will come on your head!','URLimg' => 'https://main.judgehype.com/images/froala/2017/04/1493192892_837161.jpg', 'Height' => 20, 'Weight' => 1.5, 'Color'=>'Pepe Colors','Ref' =>'K87965' ],
+    'Produit9'=>['Title'=>'Figurine of Vayne','Value' => 14.99, 'Description' =>'Figurine of League of Legends, character : Vayne.','URLimg' => 'https://rgmerch-a.akamaihd.net/products/production/1292-00-00/2018-09-07%2000:12:35.763_vayne-3603_edited_1.png', 'Height' => 20, 'Weight' => 1.5, 'Color'=>'Vayne Colors','Ref' =>'O51245' ],
+    'Produit10'=>['Title'=>'Figurine of Arhi','Value' => 14.00, 'Description' =>'Figurine of League of Lengends, character : Arhi, Skin : K/DA','URLimg' => 'https://i.imgur.com/DYLJoj8.png', 'Height' => 20, 'Weight' => 1.0, 'Color'=>'Arhi Colors','Ref' =>'I85123' ],
+    'Produit11'=>['Title'=>'Figurine of Alduin : Skyrim','Value' => 14.10, 'Description' =>'Alduin is the creator and the detroyer of the World','URLimg' => 'https://i2.cdscdn.com/pdt2/6/7/6/1/700x700/fun0849803052676/rw/figurine-skyrim-elder-scrolls-alduin-oversize-po.jpg', 'Height' => 20, 'Weight' => 1.0, 'Color'=>'Alduin Colors','Ref' =>'R66325' ],
+    'Produit12'=>['Title'=>'PlushToy Draven Poro','Value' => 29.10, 'Description' =>'Never forget to feed the Poro especially when he looks like to Draven','URLimg' => 'https://boutique-lol.com/wp-content/uploads/2018/02/peluche-poro-draven.jpg', 'Height' => 30, 'Weight' => 1.0, 'Color'=>'White','Ref' =>'N71552' ],
+   'Produit13' =>['Title'=>'Pink bear skin Fortine','Value' => 22.20, 'Description' =>'Dont wait for valentines day for having this skin : get the plushtoy !','URLimg' => 'https://www.dhresource.com/0x0s/f2-albu-g7-M01-0C-27-rBVaSVu8TM-AOSY1AAJ9YDvFCu8135.jpg/fortnite-rose-ours-en-peluche-jouet-fortnite.jpg', 'Height' => 10, 'Weight' => 1.0, 'Color'=>'Pink','Ref' =>'H88541' ],
+
+
+
+];
+
