f7c63011b1ffe1419c0ef721c32d37f9aae4ead3
Shirley Bismuth <shirley.bsth@gmail.com>
2019-03-18 12:04:36 +0100

    Voici la pull de la partie finale Figurine mise à jour avec les nouveaux tableaux merci Damien et JOYEUX ANNIVERSAIRE ps: ramène des gateaux plus souvent

---
+++ dataFigurines.php
@@ -0,0 +1,154 @@
+<?php
+$categoryProducts = [
+     'Produit1'=> [
+         'prodTitle'=>'Pink deer',
+         'prodShortTitle' => 'Pink deer',
+         'prodPrice' => 39.99,
+         'prodDescr' =>'Pink deer official plushtoy of the WCS',
+         'prodPicture' => 'https://i.imgur.com/1g4FSXj.jpg',
+         'prodCharacteristics' => ['Height' => 40,
+         'Weight' => 1.5,
+         'Color'=>'Pink',
+         'Ref' =>'S74589' ]
+         ],
+    'Produit2'=>[
+        'prodTitle'=>'Pink deer',
+        'prodShortTitle' => 'Pink deer',
+        'prodPrice' => 29.99,
+        'prodDescr' =>'Pink deer official plushtoy of the WCS',
+        'prodPicture' => 'https://i.imgur.com/NUkUBrz.jpg',
+        'prodCharacteristics' => ['Height' => 35,
+        'Weight' => 0.5,
+        'Color'=>'Pink',
+        'Ref' =>'M78445' ]
+        ],
+    'Produit3'=>[
+        'prodTitle'=>'Red deer',
+        'prodShortTitle' => 'Red deer',
+        'prodPrice' => 32.00,
+        'prodDescr' =>'Red deer official plushtoy of the WCS',
+        'prodPicture' => 'https://i.imgur.com/kWIbRsq.jpg',
+        'prodCharacteristics' => ['Height' => 30,
+        'Weight' => 1.0,
+        'Color'=>'Red',
+        'Ref' =>'K7763' ]
+        ],
+    'Produit4'=>[
+        'prodTitle'=>'White Deer',
+        'prodShortTitle' => 'White Deer',
+        'prodPrice' => 29.99,
+        'prodDescr' =>'Little white deer 100% cotton and soft',
+        'prodPicture' => 'https://i.imgur.com/yymGkE5.jpg',
+        'prodCharacteristics' => ['Height' => 3,
+        'Weight' => 1.9,
+        'Color'=>'White',
+        'Ref' =>'N53984' ]
+        ],
+    'Produit5'=>[
+        'prodTitle'=>'Rainbow lama bag',
+        'prodShortTitle' => 'Rainbow lama bag',
+        'prodPrice' => 49.99,
+        'prodDescr' =>'Wonderful rainbow lama bag for school or work !',
+        'prodPicture' => 'http://www.eispristic.fr/images/cate_3/640/Remise-Sur-Vente-Candice-guo-Super-mignon-en-peluche-jouet-beau-dessin-anim%C3%A9-arcenArpakasso-alpaga-en-peluche-poup%C3%A9e-souple-sac-%C3%A0-dos-sac-danniversaire-cadeau-De-No%C3%ABl-WJH3899-god0.jpg',
+        'prodCharacteristics' => ['Height' => 20,
+        'Weight' => 3.0,
+        'Color'=>'Rainbow',
+        'Ref' =>'G51223' ]
+        ],
+    'Produit6'=>[
+        'prodTitle'=>'PlushToy Yoda',
+        'prodShortTitle' => 'PlushToy Yoda',
+        'prodPrice' => 59.99,
+        'prodDescr' =>'PlushToy Yoda',
+        'prodPicture' => 'https://i.imgur.com/2cQHgR6.jpg',
+        'prodCharacteristics' => ['Height' => 40,
+        'Weight' => 1.5,
+        'Color'=>'Yoda Colors',
+        'Ref' =>'K87745' ]
+        ],
+    'Produit7'=>[
+        'prodTitle'=>'SpongBob',
+        'prodShortTitle' => 'SpongBob',
+        'prodPrice' => 19.99,
+        'prodDescr' =>'Who lives in a pineapple under the sea : SPONGBOB SQUARE PANTS',
+        'prodPicture' => 'https://boutique.plushtoy.fr/WebRoot/ce_fr/Shops/186372/4CE0/3BAC/86B8/EC16/296E/C0A8/8007/F5B4/peluche-bob-leponge-lunett_ml.jpg',
+        'prodCharacteristics' => ['Height' => 15,
+        'Weight' => 1.5,
+        'Color'=>'Yellow',
+        'Ref' =>'D92256' ]
+        ],
+    'Produit8'=>[
+        'prodTitle'=>'Aviator Pepe',
+        'prodShortTitle' => 'Aviator Pepe',
+        'prodPrice' => 39.99,
+        'prodDescr' =>'Find me on Dalaran and i will come on your head!',
+        'prodPicture' => 'https://main.judgehype.com/images/froala/2017/04/1493192892_837161.jpg',
+        'prodCharacteristics' => ['Height' => 20,
+        'Weight' => 1.5,
+        'Color'=>'Pepe Colors',
+        'Ref' =>'K87965' ]
+        ],
+    'Produit9'=>[
+        'prodTitle'=> 'Figurine of Vayne',
+        'prodShortTitle' => 'Figurine of Vayne',
+        'prodPrice' => 14.99,
+        'prodDescr' =>'Figurine of League of Legends, character : Vayne.',
+        'prodPicture' => 'https://rgmerch-a.akamaihd.net/products/production/1292-00-00/2018-09-07%2000:12:35.763_vayne-3603_edited_1.png',
+        'prodCharacteristics' => ['Height' => 20,
+        'Weight' => 1.5,
+        'Color'=>'Vayne Colors',
+        'Ref' =>'O51245' ]
+        ],
+    'Produit10'=>[
+        'prodTitle'=>'Figurine of Arhi',
+        'prodShortTitle' => 'Figurine of Arhi',
+        'prodPrice' => 14.00,
+        'prodDescr' =>'Figurine of League of Lengends, character : Arhi, Skin : K/DA',
+        'prodPicture' => 'https://i.imgur.com/DYLJoj8.png',
+        'prodCharacteristics' => ['Height' => 20,
+        'Weight' => 1.0,
+        'Color'=>'Arhi Colors',
+        'Ref' =>'I85123' ]
+        ],
+    'Produit11'=>[
+        'prodTitle'=>'Figurine of Alduin : Skyrim',
+        'prodShortTitle' => 'Figurine of Alduin : Skyrim',
+        'prodPrice' => 14.10,
+        'prodDescr' =>'Alduin is the creator and the detroyer of the World',
+        'prodPicture' => 'https://i2.cdscdn.com/pdt2/6/7/6/1/700x700/fun0849803052676/rw/figurine-skyrim-elder-scrolls-alduin-oversize-po.jpg',
+        'prodCharacteristics' => ['Height' => 20,
+        'Weight' => 1.0,
+        'Color'=>'Alduin Colors',
+        'Ref' =>'R66325' ]
+        ],
+    'Produit12'=>[
+        'prodTitle'=>'PlushToy Draven Poro',
+        'prodShortTitle' => 'Pokemon Slippers',
+        'prodPrice' => 29.10,
+        'prodDescr' =>'Never forget to feed the Poro especially when he looks like to Draven',
+        'prodPicture' => 'https://boutique-lol.com/wp-content/uploads/2018/02/peluche-poro-draven.jpg',
+        'prodCharacteristics' => ['Height' => 30,
+        'Weight' => 1.0,
+        'Color'=>'White',
+        'Ref' =>'N71552' ]
+        ],
+   'Produit13' =>[
+       'prodTitle'=>'Pink bear skin Fortine',
+       'prodShortTitle' => 'Pokemon Slippers',
+       'prodPrice' => 22.20,
+       'prodDescr' =>'Dont wait for valentines day for having this skin : get the plushtoy !',
+       'prodPicture' => 'https://www.dhresource.com/0x0s/f2-albu-g7-M01-0C-27-rBVaSVu8TM-AOSY1AAJ9YDvFCu8135.jpg/fortnite-rose-ours-en-peluche-jouet-fortnite.jpg',
+       'prodCharacteristics' => ['Height' => 10,
+       'Weight' => 1.0,
+       'Color'=>'Pink',
+       'Ref' =>'H88541' ]
+       ],
+
+
+
+];
+$prodCharacteristicsAlias = [ 'Height'=> 'Height',
+    'Weight' => 'Weight',
+    'Color' => 'Color',
+    'Ref' => 'Reference'];
+
