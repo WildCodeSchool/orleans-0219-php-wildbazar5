4ac9a2771e33ed2a84a13c5129f8a1d88a1e1a11
Damien Rambourg <damien.rambourg@gmail.com>
2019-03-21 18:00:34 +0100

    Fix footer container bug

--- goodies.php
+++ goodies.php
@@ -3,63 +3,63 @@
 
 <head>
     <!-- Required meta tags -->
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300i,700" rel="stylesheet" />
-    <link rel="stylesheet" href="assets/stylefonts.css" />
-    <link rel="stylesheet" href="assets/style.css" />
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300i,700" rel="stylesheet"/>
+    <link rel="stylesheet" href="assets/stylefonts.css"/>
+    <link rel="stylesheet" href="assets/style.css"/>
     <title>Wild Bazar</title>
 </head>
 
 <body>
-    <div class="container-fluid p-0">
+<div class="container-fluid p-0">
+    <?php
+    //Titre pour le jumbo
+    $titleJumbo = "goodies";
+    //Sous-Titre pour le jumbo
+    $subTitleJumbo = "Stickers, Pins and other stuff ....";
+    //Besoin d'un bouton Add neww ? mettre true
+    $addNewProductButton = true;
+    // Si bouton add new indiquer la page php contenant le formulaire d'ajout
+    $namePageNewProductForm = "adddatagoodies.php";
+    include 'header.php';
+    ?>
+</div>
+
+
+<div class="container p-0">
+    <!-- Section Products-->
+    <section class="dr_section">
         <?php
-        //Titre pour le jumbo
-        $titleJumbo = "goodies";
-        //Sous-Titre pour le jumbo
-        $subTitleJumbo = "Stickers, Pins and other stuff ....";
-        //Besoin d'un bouton Add neww ? mettre true
-        $addNewProductButton = true;
-        // Si bouton add new indiquer la page php contenant le formulaire d'ajout
-        $namePageNewProductForm = "adddatagoodies.php";
-        include 'header.php';
+        include 'assets/dataGoodies.php';
         ?>
-    </div>
+        <div class="row p-2">
 
-
-        <div class="container p-0">
-        <!-- Section Products-->
-        <section class="dr_section">
             <?php
-            include 'assets/dataGoodies.php';
+            include 'assets/cards.php';
             ?>
-            <div class="row p-2">
 
-                <?php
-                include 'assets/cards.php';
-                ?>
-
-            </div>
-
-        <?php
-        include 'footer.php';
-        ?>
+        </div>
+    </section>
+    <?php
+    include 'footer.php';
+    ?>
 
 
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
 
     <!-- Script d'effet de texte de titre majeur -->
     <script src="assets/js/scroll.js"></script>
