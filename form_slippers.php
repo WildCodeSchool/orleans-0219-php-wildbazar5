c26517d2f3d90eea716624d9eec93c434f454067
nawal.zakarya@gmail.com <nawal.zakarya@gmail.com>
2019-03-22 13:40:11 +0100

    Modif du visuel du form + ajout d'une fontion clean + boucle pour generer les checkbox

--- form_slippers.php
+++ form_slippers.php
@@ -26,45 +26,63 @@ $subTitleJumbo = 'To infinity, and beyond !';
 $addNewProductButton = false;
 require 'header.php';
 
+$materials= ['Cotton','Wool','Leather','Polyester','Velvet','Synthetic'];
 
-if (isset($_POST) && !empty($_POST)) {
-
-    $errors= [];
-
-
-    if (empty($_POST['title']) OR strlen($_POST['title']) < 10) {
-        $errors['title'] = 'Please add a title with more than 10 characters';
+function cleanInput(array $input): array
+{
+    $cleanInput = [];
+    foreach ($input as $key => $value) {
+        $cleanInput[$key] = trim($value);
+        $cleanInput[$key] = strip_tags($value);
     }
 
-    if (empty($_POST['short_title']) OR strlen($_POST['short_title']) < 10) {
-        $errors['short_title'] = 'Please add a title with more than 10 characters';
+    return $cleanInput;
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $errors = [];
+
+    $data = cleanInput($_POST);
+
+    if (empty($data['title'])) {
+        $errors['title'] = 'Please add a modal\'s title';
     }
 
-    if (empty($_POST['price'])) {
+    if (empty($data['short_title'])) {
+        $errors['short_title'] = 'Please add a card\'s title';
+    }
+
+    if (empty($data['price'])) {
         $errors['price'] = 'Please add a price to your product';
     }
 
-    if (empty($_POST['summary']) OR strlen($_POST['summary']) < 20) {
+    $summaryMinCharacters = 10;
+    if (empty($data['summary']) OR strlen($data['summary']) < $summaryMinCharacters) {
         $errors['summary'] = 'Please add a summary whit more than 20 characters';
     }
 
-    if (empty($_POST['picture'])) {
+    if (empty($data['picture'])) {
         $errors['picture'] = 'Please add a picture\'s path to your product';
     }
 
-    if (empty($_POST['size'])) {
+    if (empty($data['size'])) {
         $errors['size'] = 'Please add a size to your product';
     }
 
-    if (empty($_POST['color'])) {
+    if (empty($data['color'])) {
         $errors['color'] = 'Please add a color to your product';
     }
-    if (empty($_POST['reference']) OR !preg_match("/^[A-Z]\d{5}$/", $_POST['reference'])) {
+    if (empty($data['reference']) OR !preg_match("/^[A-Z]\d{5}$/", $data['reference'])) {
         $errors['reference'] = 'Please add a reference which begin whit a letter followed by 5 numbers';
+    }
+    if (empty($data['material'])) {
+        $errors['material'] = 'Please choose one of this options';
     }
 
     if (empty($errors)) {
-        echo "Well done !";
+        header('Location: form_slippers.php');
+        exit();
     }
 
 }
@@ -72,7 +90,7 @@ if (isset($_POST) && !empty($_POST)) {
 
 <div class="container p-5" id="nawalForm">
 
-    <form method="post" novalidate>
+    <form method="post" action="form_slippers.php#nawalForm" novalidate>
 
         <div class="row justify-content-center">
 
@@ -80,7 +98,7 @@ if (isset($_POST) && !empty($_POST)) {
                 <div class="form-group ">
                     <label for="title">Title </label>
                     <input type="text" class="form-control" id="title" aria-describedby="textHelp"
-                           placeholder="Enter the title" name="title" value="<?= $_POST['title'] ?? '' ?>" required>
+                           placeholder="Enter the title" name="title" value="<?= $data['title'] ?? '' ?>" required>
                     <p><?= $errors['title'] ?? '' ?></p>
                 </div>
             </div>
@@ -89,7 +107,7 @@ if (isset($_POST) && !empty($_POST)) {
                 <div class="form-group ">
                     <label for="short_Title">Card title </label>
                     <input type="text" class="form-control" id="short_Title" placeholder="Enter the card title"
-                           name="short_title" value="<?= $_POST['short_title'] ?? '' ?>" required>
+                           name="short_title" value="<?= $data['short_title'] ?? '' ?>" required>
                     <p><?= $errors['short_title'] ?? '' ?></p>
                 </div>
             </div>
@@ -98,7 +116,7 @@ if (isset($_POST) && !empty($_POST)) {
                 <div class="form-group ">
                     <label for="price">Price </label>
                     <input type="number" class="form-control" id="price" placeholder="0.00$" name="price"
-                           value="<?= $_POST['price'] ?? '' ?>" required>
+                           value="<?= $data['price'] ?? '' ?>" required>
                     <p><?= $errors['price'] ?? '' ?></p>
                 </div>
             </div>
@@ -107,7 +125,7 @@ if (isset($_POST) && !empty($_POST)) {
                 <div class="form-group ">
                     <label for="summary">Little description of the product </label>
                     <textarea class="form-control" id="summary" rows="4" name="summary"
-                              required><?= $_POST['summary'] ?? '' ?></textarea>
+                              required><?= $data['summary'] ?? '' ?></textarea>
                 </div>
                 <p><?= $errors['summary'] ?? '' ?></p>
 
@@ -117,7 +135,7 @@ if (isset($_POST) && !empty($_POST)) {
                 <div class="form-group">
                     <label for="picture">Picture </label>
                     <input type="text" class="form-control" id="picture" placeholder="Enter the image's path"
-                           name="picture" value="<?= $_POST['picture'] ?? '' ?>" required>
+                           name="picture" value="<?= $data['picture'] ?? '' ?>" required>
                 </div>
                 <p><?= $errors['picture'] ?? '' ?></p>
             </div>
@@ -126,37 +144,38 @@ if (isset($_POST) && !empty($_POST)) {
                 <div class="form-group">
                     <label for="size">Size </label>
                     <input type="text" class="form-control" id="size" placeholder="Enter the product's size" name="size"
-                           value="<?= $_POST['size'] ?? '' ?>" required>
+                           value="<?= $data['size'] ?? '' ?>" required>
                 </div>
                 <p><?= $errors['size'] ?? '' ?></p>
             </div>
+        </div>
 
-            <div class="col-3 py-5">
-                <div class="custom-control custom-checkbox custom-control-inline">
-                    <input type="checkbox" class="custom-control-input" id="material" name="material">
-                    <label class="custom-control-label" for="material">Cotton</label>
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <div class="card mb-3">
+                    <div class="card-header bg-transparent font-weight-bold">Materials</div>
+                    <div class="card-body text-center">
+
+                        <?php foreach ($materials as $key=> $material) : ?>
+
+                            <div class="custom-control custom-checkbox custom-control-inline py-3 pr-3">
+                                <input type="checkbox" class="custom-control-input" id="material<?=$key ?>" name="material">
+                                <label class="custom-control-label" for="material<?=$key ?>"><?= $material ?></label>
+                            </div>
+
+                        <?php endforeach; ?>
+
+                    </div>
                 </div>
             </div>
+        </div>
 
-            <div class="col-3 py-5">
-                <div class="custom-control custom-checkbox custom-control-inline">
-                    <input type="checkbox" class="custom-control-input" id="material2" name="material">
-                    <label class="custom-control-label" for="material2">Wool</label>
-                </div>
-            </div>
-
-            <div class="col-3 py-5">
-                <div class="custom-control custom-checkbox custom-control-inline">
-                    <input type="checkbox" class="custom-control-input" id="material3" name="material">
-                    <label class="custom-control-label" for="material3">Leather</label>
-                </div>
-            </div>
-
+        <div class="row justify-content-center">
             <div class="col-6 ">
                 <div class="form-group">
                     <label for="color">Color </label>
                     <input type="text" class="form-control" id="color" placeholder="Enter the product's color"
-                           name="color" value="<?= $_POST['color'] ?? '' ?>" required>
+                           name="color" value="<?= $data['color'] ?? '' ?>" required>
                 </div>
                 <p><?= $errors['color'] ?? '' ?></p>
             </div>
@@ -164,37 +183,38 @@ if (isset($_POST) && !empty($_POST)) {
             <div class="col-6 ">
                 <div class="form-group">
                     <label for="reference">Reference </label>
-                    <input type="text" class="form-control" id="reference" placeholder="Enter the product's reference"
-                           name="reference" value="<?= $_POST['reference'] ?? '' ?>" required>
+                    <input type="text" class="form-control" id="reference" pattern="^[A-Z]\d{5}$"
+                           title="add a reference which begin whit a letter followed by 5 numbers"
+                           placeholder="Enter the product's reference"
+                           name="reference" value="<?= $data['reference'] ?? '' ?>" required>
                 </div>
                 <p><?= $errors['reference'] ?? '' ?></p>
             </div>
 
-            <button type="submit" class="btn btn-primary btn-lg col-3">Submit</button>
+            <button class="btn btn-primary btn-lg col-4">Submit</button>
         </div>
 
-
-    </form>
-
+        </form>
 </div>
 
-<?php
-require 'footer.php';
-?>
+
+    <?php
+    require 'footer.php';
+    ?>
 
 
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
-<!-- Script d'effet de texte de titre majeur -->
-<script src="assets/js/scroll.js"></script>
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
+    <!-- Script d'effet de texte de titre majeur -->
+    <script src="assets/js/scroll.js"></script>
 
 </body>
 
