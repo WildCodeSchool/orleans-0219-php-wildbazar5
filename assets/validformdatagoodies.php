4ecbe1de7487c746ba8bbd9e36412fe3f970848f
Damien Rambourg <damien.rambourg@gmail.com>
2019-03-22 12:16:06 +0100

    Fix forumlaire ajout (controle serveur refactorisé) et simplification des appels php dans HTML pour les empty

--- assets/validformdatagoodies.php
+++ assets/validformdatagoodies.php
@@ -5,98 +5,80 @@
  * Date: 17/03/19
  * Time: 10:22
  */
+require 'src/functionsgeneral.php';
 
-function cleanInput($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    $data = strip_tags($data);
-    return $data;
-}
+
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $errors=[];
 
-    if (empty($_POST["productitle"])) {
+    $formData = cleanArray($_POST);
+
+    if (empty($formData["productitle"])) {
         $errors['productitle'] = "Product title is required.";
-    } else {
-        $_POST["productitle"] = cleanInput($_POST["productitle"]);
     }
-    if (empty($_POST["productshorttitle"])) {
-        if (!empty($_POST["productitle"])) {
-            $_POST['productshorttitle'] = $_POST["productitle"];
+
+    if (empty($formData["productshorttitle"])) {
+        if (!empty($formData["productitle"])) {
+            $formData['productshorttitle'] = $formData["productitle"];
         }
     }
 
-    if (empty($_POST["productprice"])) {
+    if (empty($formData["productprice"])) {
         $errors['productprice'] = "Product price is required";
     } else {
-        $_POST["productprice"] = cleanInput($_POST["productprice"]);
         // check if name only contains letters and whitespace
-        if (!preg_match("/^[0-9]*[.]?[0-9]+/",$_POST["productprice"]) ||! is_numeric($_POST["productprice"])) {
+        if (!preg_match("/^[0-9]*[.]?[0-9]+/",$formData["productprice"]) || !is_numeric($formData["productprice"])) {
             $errors['productprice'] = "For product price, only numeric with dot is allowed.";
         }
     }
-    if (empty($_POST["productpictureurl"])) {
+
+    if (empty($formData["productpictureurl"])) {
         $errors['productpictureurl'] = "Product picture url is required.";
-    } else {
-        $_POST["productpictureurl"] = cleanInput($_POST["productpictureurl"]);
     }
 
-    if (empty($_POST["productpictureurl"])) {
+    if (empty($formData["productpictureurl"])) {
         $errors['productpictureurl'] = "Product picture url is required.";
-    } else {
-        $_POST["productdescr"] = cleanInput($_POST["productpictureurl"]);
     }
 
-        $_POST["productdescr"] = cleanInput($_POST["productpictureurl"]);
-
-
-    if (empty($_POST["prodfeature"])) {
+    if (empty($formData["prodfeature"])) {
         $errors['prodfeature'] = "Product feature is required.";
-    } else {
-        $_POST["prodfeature"] = cleanInput($_POST["prodfeature"]);
     }
 
-    if (empty($_POST["prodcategory"])) {
+    if (empty($formData["prodcategory"])) {
         $errors['prodcategory'] = "Product category is required.";
-    } else {
-        $_POST["prodcategory"] = cleanInput($_POST["prodcategory"]);
     }
 
-    if (empty($_POST["prodsubcategory"])) {
-        $_POST['prodsubcategory'] = "Others";
-    } else {
-        $_POST["prodsubcategory"] = cleanInput($_POST["prodsubcategory"]);
+    if (empty($formData["prodsubcategory"])) {
+        $errors['prodcategory'] = "Product subcategory is required.";
     }
 
-    if (empty($_POST["productsize"])) {
+    if (empty($formData["productsize"])) {
         $errors['productsize'] = "Product size is required.";
     } else {
         // check if name only contains letters and whitespace
-        if (!preg_match("/^[0-9]*[.]?[0-9]+/",$_POST["productsize"]) ||
-            ! is_numeric($_POST["productsize"])) {
+        if (!preg_match("/^[0-9]*[.]?[0-9]+/",$formData["productsize"]) ||
+            ! is_numeric($formData["productsize"])) {
             $errors['productsize'] = "For product size, only numeric with dot is allowed.";
         }
     }
 
-    if (empty($_POST["productcolor"])) {
+    if (empty($formData["productcolor"])) {
         $errors['productcolor'] = "Product color is required.";
-    } else {
-        $_POST["productcolor"] = cleanInput($_POST["productcolor"]);
     }
 
-    if (empty($_POST["prodreference"])) {
+    if (empty($formData["prodreference"])) {
         $errors['prodreference'] = "Product reference is required.";
     } else {
         // check if name only contains letters and whitespace
-        if (!preg_match("/^[A-Z]\d{5}$/",$_POST["prodreference"])) {
+        if (!preg_match("/^[A-Z]\d{5}$/",$formData["prodreference"])) {
             $errors['prodreference'] = "For product reference, only numeric with dot is allowed.";
         }
     }
 
 
     if (count($errors) == 0) {
+
         header("Location: adddatagoodies_confirm.php");
         exit();
     }
